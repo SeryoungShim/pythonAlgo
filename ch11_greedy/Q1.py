c9000f9619d948eaf6d2652348ff501da6cc099b
SeryoungShim <csssr1207@gmail.com>
2020-09-14 22:12:56 +0900

    Q1

--- ch11_greedy/Q1.py
+++ ch11_greedy/Q1.py
@@ -1,2 +1,21 @@
 def main():
-    return
+    N = int(input())
+    lst = list(map(int, input().split()))
+    lst = sorted(lst)
+
+    cnt = 0
+    i = -1
+    j = 0
+    while(i < len(lst) and j < len(lst)):
+        while(j < len(lst)):
+            if j - i >= lst[j]:
+                cnt += 1
+                i = j
+                j += 1
+                print(f"j : {lst[j]}")
+                break
+            else:
+                j += 1
+    print(cnt)
+
+main()
